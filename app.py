cfdc1a2bc2a12de2c77d266f74fe58f7591e1935
Rob Beal <296433+robertbeal@users.noreply.github.com>
2019-09-21 22:08:05 +0100

    Strip the app back to the basics

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from reader import Reader
+import os
+import socket
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
+from urllib.parse import urlparse
 import signal
 
 
@@ -15,13 +17,15 @@ class App:
 
 
 if __name__ == "__main__":
-    reader = Reader(SimpleMFRC522())
+    reader = SimpleMFRC522()
 
     try:
         while True:
-            action = reader.read()
+            id = reader.read()[0]
 
-            if action:
-                action.execute()
+            ip = socket.gethostbyname("hifi.rb.kohi.uk")
+            cards = {"8115724414": "http://server.rb.kohi.uk:8200/MediaItems/17356.mp3"}
+
+            os.system("python dlnap.py --ip {} --play {}".format(ip, cards[id]))
     except KeyboardInterrupt:
         GPIO.cleanup()
